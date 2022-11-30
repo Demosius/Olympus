a093089dc196d03ea9b72e744f4f60f7d33dc68f
Demosius <demonic_cure@hotmail.com>
2022-11-30 16:29:04 +1000

    Panacea: Begin Ptential Negative Check functionality.

---
+++ Panacea/ViewModels/Components/PotentialNegativeVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Panacea.ViewModels.Commands;
+using Uranus;
+using Uranus.Annotations;
+
+namespace Panacea.ViewModels.Components;
+
+public class PotentialNegativeVM : INotifyPropertyChanged
+{
+    public Helios Helios { get; set; }
+
+
+    #region Commands
+
+    public RunPotentialNegativesCheckCommand RunPotentialNegativesCheckCommand { get; set; }
+
+    #endregion
+
+    public PotentialNegativeVM(Helios helios)
+    {
+        Helios = helios;
+    }
+    
+    public void RunPotentialNegativesCheck()
+    {
+        throw new NotImplementedException();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
